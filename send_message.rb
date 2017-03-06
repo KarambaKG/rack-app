10c5f6eee310675da1d81da70516cf8d4b86f570
Omurbek Choibekov <omka3s@gmail.com>
2017-03-06 18:22:56 +0600

    replacing variables by value in templates method enhanced

--- send_message.rb
+++ send_message.rb
@@ -1,6 +1,7 @@
 require 'rack'
 require 'json'
 require './template'
+require 'erb'
 class SendMessage
 
    def initialize(result)
@@ -10,17 +11,23 @@ class SendMessage
            tip = tempHash.typ
            lang = tempHash.lang
            temp = File.read("templates/#{tip}_#{lang}.json")
-           qw = tempHash.code
-           temp1 = JSON.parse(temp)
-           temp1.each do |k,v|        
-            temp1[k] = v % ["#{qw}"]
+           code = tempHash.code
+           filtered_data = temp.gsub("[code]", "#{code}")
+           File.open("result_#{tip}_#{lang}.json","w") do |f|
+          #  File.open("templates/#{tip}_#{lang}.json","w") do |f|
+            f.write(filtered_data)
           end
-          hash = {}
-	  tempHash.instance_variables.each {|var| hash[var.to_s.delete("@")] = tempHash.instance_variable_get(var) }
+          #  temp1 = JSON.parse(temp)
+          #  temp1.each do |k,v|        
+          #   temp1[k] = v % ["#{code}"]
+            
+          # end
+   #        hash = {}
+	  # tempHash.instance_variables.each {|var| hash[var.to_s.delete("@")] = tempHash.instance_variable_get(var) }
 
-           lastHash = hash.merge(temp1)
-           file = File.open("result_#{tip}_#{lang}.json","a"){ |f| f << lastHash.to_json }
-           file.close
-       return lastHash
+   #         lastHash = hash.merge(temp1)
+   #         file = File.open("result_#{tip}_#{lang}.json","a"){ |f| f << lastHash.to_json }
+   #         file.close
+       return filtered_data
    end
 end
