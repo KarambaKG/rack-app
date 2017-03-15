a8901421ae69fefe114617447beca9dd2042238a
Omurbek Choibekov <omka3s@gmail.com>
2017-03-15 16:49:53 +0600

    qwqwqw

--- test_post.rb
+++ test_post.rb
@@ -6,16 +6,18 @@ def create_agent
 	
 	begin
 		# "192" "168" "40" "63"
-		@w = ARGV[0].to_i
-		@w1 = ARGV[1].to_i
-		@w2 = ARGV[2].to_i
-		@w3 = ARGV[3].to_i
-		var = rand(0...1000)
-	     uri = URI("http://#{@w}.#{@w1}.#{@w2}.#{@w3}:9292/new")
-	    # uri = URI("http://192.168.40.63:9292/new")
+		# @w = ARGV[0].to_i
+		# @w1 = ARGV[1].to_i
+		# @w2 = ARGV[2].to_i
+		# @w3 = ARGV[3].to_i
+		lang = ARGV[0]
+		typ = ARGV[1]
+		var = rand(0..10000)
+	     # uri = URI("http://#{@w}.#{@w1}.#{@w2}.#{@w3}:9292/new")
+	    uri = URI("http://192.168.40.63:9292/new")
 	    http = Net::HTTP.new(uri.host, uri.port)
 	    req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
-	    req.body = {'lang' => 'en', 'typ' => 'sms','phone_number'=> '232323','code' => "#{var}"}.to_json
+	    req.body = {'lang' => "#{lang}", 'typ' => "#{typ}",'phone_number'=> '232323','code' => "#{var}"}.to_json
 	    res = http.request(req)
 	    return res
 	rescue => e
