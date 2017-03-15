c11afb74dc7596d5c0eb81580459e60bcc544264
Omurbek Choibekov <omka3s@gmail.com>
2017-03-15 14:39:45 +0600

    qw

--- test_post.rb
+++ test_post.rb
@@ -6,13 +6,13 @@ def create_agent
 	
 	begin
 		# "192" "168" "40" "63"
-		# @w = ARGV[0].to_i
-		# @w1 = ARGV[1].to_i
-		# @w2 = ARGV[2].to_i
-		# @w3 = ARGV[3].to_i
+		@w = ARGV[0].to_i
+		@w1 = ARGV[1].to_i
+		@w2 = ARGV[2].to_i
+		@w3 = ARGV[3].to_i
 
-	    # uri = URI("http://#{@w}.#{@w1}.#{@w2}.#{@w3}:9292/new")
-	    uri = URI("http://192.168.40.63:9292/new")
+	     uri = URI("http://#{@w}.#{@w1}.#{@w2}.#{@w3}:9292/new")
+	    # uri = URI("http://192.168.40.63:9292/new")
 	   
 	    http = Net::HTTP.new(uri.host, uri.port)
 	    req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
