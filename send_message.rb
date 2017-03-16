d60b15e3da26eb6513571d81dbe3bedffa81cc5f
Maxim <karamba92@mail.ru>
2017-03-16 16:30:11 +0600

    #19 добавил вывод в консоль на сервере и консольном приложении сообщений об успешных и провальных операциях

--- send_message.rb
+++ send_message.rb
@@ -10,14 +10,19 @@ class SendMessage
   end
 
   def self.detect(tempHash)
-   tip = tempHash['typ']
-   lang = tempHash['lang']
-   temp = File.read("templates/#{tip}_#{lang}.json")
-   tempHash.each do |k,v| temp.gsub!(k,v)
-   end
-   File.open("result_#{tip}_#{lang}.json","w") do |f|
-    f.write(temp)
-   end
-    return temp
+    begin
+     tip = tempHash['typ']
+     lang = tempHash['lang']
+     temp = File.read("templates/#{tip}_#{lang}.json")
+    rescue => error
+      print "Присланный json не соответствует шаблонам на web-сервере. Добавьте новый шаблон в админке, чтобы обработать присланные параметры."
+    end
+    tempHash.each do |k,v| temp.gsub!(k,v)
+    end
+    File.open("result_#{tip}_#{lang}.json","w") do |f|
+      f.write(temp)
+      print "Сообщение с присланными параметрами успешно сохранено. "
+    end
+      return temp
   end
 end
--- test_post.rb
+++ test_post.rb
@@ -1,16 +1,19 @@
 require 'net/http'
 require 'json'
+require 'byebug'
 
 def create_agent
-	begin
-	    uri = URI('http://localhost:9292/new')
-	    http = Net::HTTP.new(uri.host, uri.port)
-	    req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
-	    req.body = {'lang' => 'kz', 'typ' => 'smsotp', 'CODE' => '112as'}.to_json
-	    res = http.request(req)
-	    return res
-	rescue => e
-	    puts "failed #{e}"
+	uri = URI('http://localhost:9292/new')
+	http = Net::HTTP.new(uri.host, uri.port)
+	req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+	req.body = {'lang' => 'ru', 'typ' => 'smsnewpwd', 'CODE' => '11'}.to_json
+	res = http.request(req)
+	if res.kind_of? Net::HTTPSuccess
+		p "JSON параметры успешно отправлены"
+		p res
+	else
+		p 'ups! Что то пошло не так('
+		p res
 	end
 end
 create_agent
