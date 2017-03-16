43c1fb7901c1f0a148b196c0cde22f47939c220e
Maxim <karamba92@mail.ru>
2017-03-16 13:11:11 +0600

    #18 метод детект теперь работает правильно со всеми шаблонами

--- send_message.rb
+++ send_message.rb
@@ -2,18 +2,19 @@ require 'rack'
 require 'json'
 require './template'
 require 'erb'
+
 class SendMessage
 
   def initialize(result)
    @result = result
   end
+
   def self.detect(tempHash)
    tip = tempHash['typ']
    lang = tempHash['lang']
    temp = File.read("templates/#{tip}_#{lang}.json")
-   code = tempHash['code']
-   map = {"[code]"=> "#{code}", "[tip]"=> "#{tip}"}
-   map.each {|k,v| temp.sub!(k,v)}
+   tempHash.each do |k,v| temp.gsub!(k,v)
+   end
    File.open("result_#{tip}_#{lang}.json","w") do |f|
     f.write(temp)
    end
