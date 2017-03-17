d4ac8f9061076fd23f7f8e405334ad2909de559a
Maxim <karamba92@mail.ru>
2017-03-17 14:40:49 +0600

    12312312

--- test_post.rb
+++ test_post.rb
@@ -6,7 +6,7 @@ def create_agent
 	    uri = URI('http://192.168.40.90:9292/new')
 	    http = Net::HTTP.new(uri.host, uri.port)
 	    req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
-	    req.body = {'lang' => 'us', 'typ' => 'email','phone_number'=> '232323','code' => 'maxim'}.to_json
+	    req.body = {'lang' => 'ru', 'typ' => 'smsotp','phone_number'=> '232323','code' => 'maxim123'}.to_json
 	    res = http.request(req)
 	    return res
 	rescue => e
