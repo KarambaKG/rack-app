a43a1012cdd4a039c037b5557b924573b036dc3a
Maxim <karamba92@mail.ru>
2017-03-22 18:49:45 +0600

    #15 ARGV принимает все параметры и айпи

--- lib/client/test_post.rb
+++ lib/client/test_post.rb
@@ -4,16 +4,17 @@ require 'byebug'
 require 'pry'
 
 def create_agent
-
+	# binding.pry
 	all_params = Hash.new
 	ARGV.each {|arg| c=arg.split('='); all_params[c[0]]=c[1] }
-	uri = URI('http://localhost:9292/new')
+	remote_host = all_params['ip']
+	uri = URI("http://#{remote_host}:9292/new")
 	http = Net::HTTP.new(uri.host, uri.port)
 	req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
 	req.body = all_params.to_json
 	res = http.request(req)
 	if res.kind_of? Net::HTTPSuccess
-		p "JSON параметры успешно отправлены"
+		p 'JSON параметры успешно отправлены'
 		p res
 	else
 		p 'ups! Что то пошло не так('
