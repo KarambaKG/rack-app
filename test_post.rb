2db39d513d9c2cab6ebe9d4a22b896b95fe0f30d
Maxim <karamba92@mail.ru>
2017-03-22 17:29:21 +0600

    Последний АРГВ должен быть айпи адрессом

--- test_post.rb
+++ test_post.rb
@@ -2,16 +2,15 @@ require 'net/http'
 require 'json'
 
 def create_agent
-	ip_address = ARGV[1]
 	all_params = Hash.new
 	ARGV.each {|arg| c=arg.split('='); all_params[c[0]]=c[1] }
-	uri = URI("http://#{ip_address}:9292/new")
+	uri = URI("http://#{ARGV.last}:9292/new")
 	http = Net::HTTP.new(uri.host, uri.port)
 	req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
 	req.body = all_params.to_json
 	res = http.request(req)
 	if res.kind_of? Net::HTTPSuccess
-		p "JSON параметры успешно отправлены"
+		p 'JSON параметры успешно отправлены'
 		p res
 	else
 		p 'ups! Что то пошло не так('
