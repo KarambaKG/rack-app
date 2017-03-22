f24fd1ca8b1d43bbebe9c0f0816f1c3eebcacaf7
Maxim <karamba92@mail.ru>
2017-03-22 14:18:43 +0600

    Обновил консольное приложение. Проверка ARGV в докере ip addressom

--- test_post.rb
+++ test_post.rb
@@ -1,16 +1,24 @@
 require 'net/http'
 require 'json'
+require 'byebug'
+require 'pry'
 
 def create_agent
-	begin
-	    uri = URI('http://192.168.40.90:9292/new')
-	    http = Net::HTTP.new(uri.host, uri.port)
-	    req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
-	    req.body = {'lang' => 'ru', 'typ' => 'smsotp','phone_number'=> '232323', 'CODE' => 'maxim123'}.to_json
-	    res = http.request(req)
-	    return res
-	rescue => e
-	    puts "failed #{e}"
+	ip_address = ARGV[1]
+	all_params = Hash.new
+	ARGV.each {|arg| c=arg.split('='); all_params[c[0]]=c[1] }
+	uri = URI("http://#{ip_address}:9292/new")
+	http = Net::HTTP.new(uri.host, uri.port)
+	req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+	req.body = all_params.to_json
+	res = http.request(req)
+	if res.kind_of? Net::HTTPSuccess
+		p "JSON параметры успешно отправлены"
+		p res
+	else
+		p 'ups! Что то пошло не так('
+		p res
 	end
+
 end
 create_agent
