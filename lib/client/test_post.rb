e17214de33246ace881f76bb8ac4c355d386be64
Maxim <karamba92@mail.ru>
2017-03-22 14:01:28 +0600

    #15 настроили ARGV переменные, теперь в консоли можно вводить любые переменные шаблонов

--- lib/client/test_post.rb
+++ lib/client/test_post.rb
@@ -1,16 +1,16 @@
 require 'net/http'
 require 'json'
 require 'byebug'
+require 'pry'
 
 def create_agent
-	lang = ARGV[0]
-	typ = ARGV[1]
-	var = rand(0..10000)
+
+	all_params = Hash.new
+	ARGV.each {|arg| c=arg.split('='); all_params[c[0]]=c[1] }
 	uri = URI('http://localhost:9292/new')
 	http = Net::HTTP.new(uri.host, uri.port)
 	req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
-
-	req.body = {'lang' => "#{lang}", 'typ' => "#{typ}",'CODE' => "#{var}",'phone_number'=> '232323'  }.to_json
+	req.body = all_params.to_json
 	res = http.request(req)
 	if res.kind_of? Net::HTTPSuccess
 		p "JSON параметры успешно отправлены"
@@ -19,6 +19,6 @@ def create_agent
 		p 'ups! Что то пошло не так('
 		p res
 	end
+
 end
 create_agent
-
